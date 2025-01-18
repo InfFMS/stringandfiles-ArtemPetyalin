165395541a7713d5e4db53c235538e2416adbbbd
Петялин Артём Дмитриевич <a.petyalin@main.nsu.ru>
2025-01-18 14:16:09 +0700

    Здравствуйте! Сделал вторую задачку. Третью еще не доделал

--- task3.py
+++ task3.py
@@ -6,3 +6,54 @@
 # слово2: количество
 #
 # Убедитесь, что слова записаны в алфавитном порядке.
+import re
+f = open('task3.txt', encoding='utf-8')
+text = f.read().lower()
+text = re.split(r'[ .,\n]+', text)
+text.sort()
+print(text)
+text2 = text
+f.close()
+
+unic_list = []
+i = 0
+
+while i < len(text) - 1:
+
+    if text[i] == '':
+        text.pop(i)
+
+    else:
+        j = i + 1
+
+        while j < len(text):
+
+            if text[i] == text[j]:
+                text.pop(j)
+
+            j += 1
+
+    i += 1
+
+amounts = []
+
+for i in range(len(text)):
+    counter = 0
+
+    for j in range(len(text2)):
+
+        if text[i] == text2[j]:
+            counter += 1
+
+    amounts.append(counter)
+
+answer = []
+
+f = open('task3answer.txt', 'x', encoding='utf-8')
+
+for i in range(len(amounts)):
+    stringichek = text[i] + ' - ' + str(amounts[i]) +  ' \n'
+    f.write(stringichek)
+
+f.read()
+f.close()
